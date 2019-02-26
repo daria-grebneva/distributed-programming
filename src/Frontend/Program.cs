6aa4e040db0f840c117b0f8c25ad2740ffdfb52c
daria-grebneva <greda2598@gmail.com>
2019-02-26 23:29:57 +0300

    Конфликтный файл

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -19,6 +19,7 @@ namespace Frontend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5001")
                 .UseStartup<Startup>();
     }
 }
